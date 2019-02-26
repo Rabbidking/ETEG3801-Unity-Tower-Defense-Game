05112db3bef93ac4b670c07eb87adb6fb6f35ed6
Aleks <Aleks@LAPTOP-6F0C3OGI>
2019-02-26 13:06:15 -0500

    Can now only build on selected tiles. Left Mouse Button to select a tile. Right Mouse Button to deselect a selected tile.

--- Assets/Scripts/BuildMap.cs
+++ Assets/Scripts/BuildMap.cs
@@ -20,14 +20,16 @@ public class BuildMap : MonoBehaviour
     public Camera MapCamera;
     public int MapLayer;
 
-    public GameObject MapCursor, TowerParent, MapTerrain;
+    public GameObject MapCursor, MapCursorSelect, TowerParent, MapTerrain;
     private MeshRenderer MapCursorRenderer;
 
     private int MapSize, MapGridSize;
     private float MapHeight;
 
-    private uint[] CurrentTile = new uint[2];  // [x, z]
-    private float[] BottomLeft = new float[2]; // [x, z]
+    private bool TileIsSelected = false;
+    private uint[] CursorTile   = new uint[2];  // [x, z]
+    private uint[] SelectedTile = new uint[2];  // [x, z]
+    private float[] BottomLeft  = new float[2]; // [x, z]
     private float TileSize;
 
     public void SetMapValues(int mapSize, int mapGridSize, float mapHeight)
@@ -46,28 +48,43 @@ public class BuildMap : MonoBehaviour
         BottomLeft[1] = (float)MapSize / -2.0f;
 
         Vector3 lp = MapCursor.transform.localPosition;
-        MapCursor.transform.localPosition = new Vector3(lp.z, MapHeight + 0.0125f, lp.z);
-        MapCursor.transform.localScale = new Vector3(TileSize * 0.95f, 0.025f, TileSize * 0.95f);
+        MapCursor.transform.localPosition = new Vector3(lp.x, MapHeight + 0.0125f, lp.z);
+        MapCursor.transform.localScale    = new Vector3(TileSize * 0.95f, 0.025f, TileSize * 0.95f);
         MapCursorRenderer = MapCursor.GetComponent<MeshRenderer>();
+        MapCursor.SetActive(false);
+
+        MapCursorSelect.transform.localPosition = new Vector3(lp.x, MapHeight + 0.005f, lp.z);
+        MapCursorSelect.transform.localScale = new Vector3(TileSize * 0.93f, 0.01f, TileSize * 0.93f);
+        MapCursorSelect.SetActive(false);
 
         MapCamera.transform.position = new Vector3(0, MapSize, 0);
         MapCamera.transform.rotation.SetLookRotation(new Vector3(90.0f, 0, 0));
     }
 
-    // (Input.GetMouseButtonDown(0) //
-
     void Update()
     {
         if(RaycastToTile())
         {
-            HighlightCurrentTile();
-            if(Input.GetKeyDown("b"))
-                SpawnTower();
-            else if(Input.GetKeyDown("d"))
-                DestroyTower();
+            HighlightCursorTile();
+
+            if(Input.GetMouseButtonDown(0))
+                SelectCursorTile();
         }
         else
             MapCursor.SetActive(false);
+
+        if(TileIsSelected)
+        {
+            if(Input.GetMouseButtonDown(1))
+                DeselectTile();
+            else
+            {
+                if(Input.GetKeyDown("b"))
+                    SpawnTower();
+                else if(Input.GetKeyDown("d"))
+                    DestroyTower();
+            }
+        }
     }
 
     private bool RaycastToTile()
@@ -77,29 +94,52 @@ public class BuildMap : MonoBehaviour
 
         if(Physics.Raycast(cameraRay, out rayHit, (float)MapCamera.transform.position.y * 1.2f, 1 << MapLayer))
         {
-            CurrentTile[1] = (uint)((rayHit.point.x + -BottomLeft[1]) / TileSize);
-            CurrentTile[0] = (uint)((rayHit.point.z + -BottomLeft[0]) / TileSize);
+            CursorTile[1] = (uint)((rayHit.point.x + -BottomLeft[1]) / TileSize);
+            CursorTile[0] = (uint)((rayHit.point.z + -BottomLeft[0]) / TileSize);
             return true;
         }
 
         return false;
     }
 
-    private void GetCurrentTileCenter(out float centerX, out float centerZ)
+    public void GetTileCenter(uint tileX, uint tileZ, out float centerX, out float centerZ)
     {
-        centerX = BottomLeft[1] + (TileSize * (CurrentTile[1] + 0.5f));
-        centerZ = BottomLeft[0] + (TileSize * (CurrentTile[0] + 0.5f));
+        centerX = BottomLeft[1] + (TileSize * (tileX + 0.5f));
+        centerZ = BottomLeft[0] + (TileSize * (tileZ + 0.5f));
     }
 
-    private void HighlightCurrentTile()
+    private void DeselectTile()
+    {
+        MapCursorSelect.SetActive(false);
+        TileIsSelected = false;
+    }
+
+    private void SelectCursorTile()
+    {
+        if(Tiles[CursorTile[0], CursorTile[1]] != TILE_TYPE.PATH)
+        {
+            SelectedTile[0] = CursorTile[0];
+            SelectedTile[1] = CursorTile[1];
+
+            float centerX, centerZ;
+            GetTileCenter(SelectedTile[1], SelectedTile[0], out centerX, out centerZ);
+
+            MapCursorSelect.transform.localPosition = new Vector3(centerX, MapHeight + 0.005f, centerZ);
+            MapCursorSelect.SetActive(true);
+
+            TileIsSelected = true;
+        }
+    }
+
+    private void HighlightCursorTile()
     {
         float newCursorX, newCursorZ;
-        GetCurrentTileCenter(out newCursorX, out newCursorZ);
+        GetTileCenter(CursorTile[1], CursorTile[0], out newCursorX, out newCursorZ);
 
         MapCursor.SetActive(true);
         MapCursor.transform.position = new Vector3(newCursorX, MapCursor.transform.position.y, newCursorZ);
 
-        if(Tiles[CurrentTile[0], CurrentTile[1]] == 0)
+        if(Tiles[CursorTile[0], CursorTile[1]] != TILE_TYPE.PATH)
             MapCursorRenderer.material.color = Color.blue;
         else
             MapCursorRenderer.material.color = Color.red;
@@ -107,32 +147,32 @@ public class BuildMap : MonoBehaviour
 
     private void SpawnTower()
     {
-        if(Tiles[CurrentTile[0], CurrentTile[1]] == TILE_TYPE.BUILDABLE)
+        if(Tiles[SelectedTile[0], SelectedTile[1]] == TILE_TYPE.BUILDABLE)
         {
             TileObject newTower;
-            newTower.mTileX = CurrentTile[0];
-            newTower.mTileY = CurrentTile[1];
+            newTower.mTileX = SelectedTile[0];
+            newTower.mTileY = SelectedTile[1];
 
             float newX, newZ;
-            GetCurrentTileCenter(out newX, out newZ);
+            GetTileCenter(SelectedTile[0], SelectedTile[1], out newX, out newZ);
 
             newTower.mGameObject = Instantiate(TowerPrefabs.DummyTower);
             newTower.mGameObject.transform.SetParent(TowerParent.transform, false);
-            newTower.mGameObject.transform.localPosition = new Vector3(newX, 0.5f, newZ);
-            newTower.mGameObject.transform.localScale = new Vector3(TileSize * 0.9f, MapHeight + 0.5f, TileSize * 0.9f);
+            newTower.mGameObject.transform.localPosition = new Vector3(newZ, MapHeight + 0.5f, newX);
+            newTower.mGameObject.transform.localScale = new Vector3(TileSize * 0.9f, 1, TileSize * 0.9f);
             TileObjects.Add(newTower);
 
-            Tiles[CurrentTile[0], CurrentTile[1]] = TILE_TYPE.TAKEN;
+            Tiles[SelectedTile[0], SelectedTile[1]] = TILE_TYPE.TAKEN;
         }
     }
 
     private void DestroyTower()
     {
-        if(Tiles[CurrentTile[0], CurrentTile[1]] == TILE_TYPE.TAKEN)
+        if(Tiles[SelectedTile[0], SelectedTile[1]] == TILE_TYPE.TAKEN)
         {
             foreach(TileObject TO in TileObjects)
             {
-                if(TO.mTileX == CurrentTile[0] && TO.mTileY == CurrentTile[1])
+                if(TO.mTileX == SelectedTile[0] && TO.mTileY == SelectedTile[1])
                 {
                     Destroy(TO.mGameObject);
                     TileObjects.Remove(TO);
@@ -140,7 +180,7 @@ public class BuildMap : MonoBehaviour
                 }
             }
 
-            Tiles[CurrentTile[0], CurrentTile[1]] = TILE_TYPE.BUILDABLE;
+            Tiles[SelectedTile[0], SelectedTile[1]] = TILE_TYPE.BUILDABLE;
         }
     }
 }
