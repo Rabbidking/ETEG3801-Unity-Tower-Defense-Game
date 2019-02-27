1d33c8bf5f98d0f8e6046a967ed5ae67b2d485ac
Matthew Crabtree <mdcrabtree2@gmail.com>
2019-02-27 17:25:03 -0500

    i got agent pathfinding to work finally

--- Assets/Scripts/NavMeshGen.cs
+++ Assets/Scripts/NavMeshGen.cs
@@ -15,8 +15,9 @@ public class NavMeshGen : MonoBehaviour
 		List<NavMeshBuildSource> sourceList = new List<NavMeshBuildSource>();
 		List<NavMeshBuildMarkup> markups = new List<NavMeshBuildMarkup>();
 		NavMeshBuilder.CollectSources(new Bounds(Vector3.zero, Vector3.one * 1000), 0 | (1 << 9), NavMeshCollectGeometry.RenderMeshes, 0 , markups, sourceList);
-		
-		NavMeshData nmd = NavMeshBuilder.BuildNavMeshData(NavMesh.CreateSettings(),sourceList, new Bounds(transform.position, Vector3.one * 1000), Vector3.zero, Quaternion.identity);
+		NavMeshBuildSettings nmbs = NavMesh.CreateSettings();
+		nmbs.agentTypeID = Spawner.nma.agentTypeID;
+		NavMeshData nmd = NavMeshBuilder.BuildNavMeshData(nmbs,sourceList, new Bounds(transform.position, Vector3.one * 1000), Vector3.zero, Quaternion.identity);
 		if (!nmd) { print("OH NO"); return; }
 		NavMesh.AddNavMeshData(nmd);
 		Spawner spawn = Instantiate(Spawner, mg.Start, Quaternion.identity);
