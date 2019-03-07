38520b06e5d56847883a7136cd1648f7ec55f60f
Rabbidking <bickel.john@yahoo.com>
2019-03-07 12:56:37 -0500

    Piercing Bullets are a go!

---
+++ Assets/Scripts/PiercingBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiercingBullet : Bullet
+{
+    public int numPierce = 2;
+
+    void OnTriggerEnter(Collider co)
+    {
+        if(co.tag == "Enemy")
+        {
+            numPierce--;
+            print(co);
+            Health health = co.GetComponentInChildren<Health>();
+            if (health)
+            {
+                health.decrease();
+                if (numPierce <= 0)
+                {
+                    numPierce = 0;
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
